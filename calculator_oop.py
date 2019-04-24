e15396fd9d5ad3ec86d930db459b30cb731b84f0
Tahsin Khan <tahsin_k@live.co.uk>
2019-04-24 14:46:19 +0100

    added unit testing to calculator

--- calculator_oop.py
+++ calculator_oop.py
@@ -6,29 +6,39 @@ class Calculator:
 
     def addition(self):
         result = self.input_num_1 + self.input_num_2
+        print(type(result) == int)      ##Addition result unit test
         return 'Your result is: ' + str(result)
 
     def subtract(self):
         result = self.input_num_1 - self.input_num_2
+        print(type(result) == int)      ##Subtract result unit test
         return 'Your result is: ' + str(result)
 
     def multiply(self):
         result = self.input_num_1 * self.input_num_2
+        print(type(result) == int)      ##Multiply result unit test
         return 'Your result is: ' + str(result)
 
     def divide(self):
         result = self.input_num_1 / self.input_num_2
+        print(type(result) == int)      ##Divide result unit test
         return 'Your result is: ' + str(result)
 
     def user_input_calculator(self):
-        # self.input_num_1 = input_num_1
-        # self.input_num_2 = input_num_2
-        # self.operation = operation
+        ## self.input_num_1 = input_num_1
+        ## self.input_num_2 = input_num_2
+        ## self.operation = operation
 
         while True:
             self.input_num_1 = int(input('Enter your first number: '))
             self.input_num_2 = int(input('Enter your second number: '))
             self.operation = input('What would you like to do to the numbers? (Add, Subtract, Multiply or Divide only): ')
+
+
+            ## Input unit testing
+            print(type(self.input_num_1) == int)
+            print(type(self.input_num_2) == int)
+            print(type(self.operation) == str)
 
             #Add
             if self.operation.strip().capitalize() == 'Add':
@@ -48,6 +58,6 @@ class Calculator:
             else:
                 print('Please state one of the specified options (Add, Subtract, Multiply or Divide)')
 
-
+# Class instance
 calculate = Calculator()
 calculate.user_input_calculator()
